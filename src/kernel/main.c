ba8087bea5c6b9df2c4ad7f5197372ab23179fc1
Tom Everett <tom@bernice.khubla.local>
2014-10-14 18:44:31 -0600

    refactoring

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,9 +3,9 @@
 
 void main( void )
 {
-    char *str = "Hello from FemtoOs";
-//	clear();
-	monitor_write(str);
+	monitor_write( "Hello from FemtoOs\n");
+	monitor_write( "tge\n");
 
-  for(;;); /* Keep the OS running */
+
+	for(;;); /* Keep the OS running */
 }
