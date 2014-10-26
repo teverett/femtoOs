d1e26536dda5828e6165708683baeb5fdf08fce5
Tom Everett <tom@khubla.com>
2014-10-26 10:47:56 -0600

    more docs

--- README.md
+++ README.md
@@ -3,6 +3,15 @@ femtoOs
 
 A trivial i386 protected mode operating system
 
+Requirements
+----------
+
+femtoOs builds on FreeBSD.  You will need these packages
+
+* NASM
+* GRUB2
+* cdrtools
+
 Building
 ----------
 
--- build.sh
+++ build.sh
@@ -2,13 +2,26 @@
 set -e
 
 IMAGE=femtoos.img
+ISO=femtoos.iso
 TMP=/tmp/femtoos
-IMAGESIZE=2880
+IMAGESIZE=10000
 
+#
+# remove old file
+#
 rm -f $IMAGE
+rm -f $ISO
+
+#
+# build kernel
+#
 make clean
 make
-dd bs=512 count=$IMAGESIZE of=$IMAGE if=/dev/zero
+
+#
+# make .img file, with the kernel and with grub
+#
+dd bs=1k count=$IMAGESIZE of=$IMAGE if=/dev/zero
 mdconfig -a -t vnode -f $IMAGE -u 0
 gpart create -s MBR  /dev/md0
 gpart add -t fat32 -b 512k /dev/md0
@@ -19,6 +32,15 @@ mount_msdosfs /dev/md0s1 $TMP
 grub-install --boot-directory=$TMP/boot/ --allow-floppy --modules="normal part_msdos multiboot" /dev/md0 
 cp src/kernel/kernel.elf $TMP/boot/kernel
 cp src/grub/* $TMP/boot/grub/
+
+#
+# make .iso file
+#
+#mkisofs -v -R -J -iso-level 3 -o $ISO $TMP
+
+#
+# unmount 
+#
 umount $TMP
 mdconfig -u 0 -d
 
